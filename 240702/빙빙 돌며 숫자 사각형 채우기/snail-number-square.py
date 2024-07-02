ae32ab0f71d7c36c43ebd042ee2bddc07097437e
nayoung <ask5046a@gmail.com>
2024-07-02 18:45:40 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 134ms, Memory: 25MB, Status: Passed - CodeTree

--- 240702/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
+++ 240702/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
@@ -6,13 +6,13 @@ n, m = map(int, input().split())
 
 #답안
 answer = [
-    [0] * n
-    for _ in range(m)
+    [0] * m
+    for _ in range(n)
 ]
 
 #범위 내 확인
 def in_range(x, y):
-    return 0 <= x and x < m and 0 <= y and y < n
+    return 0 <= x and x < n and 0 <= y and y < m
 
 #그래프
 dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
