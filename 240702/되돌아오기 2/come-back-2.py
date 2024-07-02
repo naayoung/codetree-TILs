6ae58d1626e9d75a3e0cfb6edf790a94817e1410
nayoung <ask5046a@gmail.com>
2024-07-02 19:53:30 +0900

    Title: 되돌아오기 2, Time: 126ms, Memory: 25MB, Status: Passed - CodeTree

--- 240702/되돌아오기 2/come-back-2.py
+++ 240702/되돌아오기 2/come-back-2.py
@@ -2,24 +2,26 @@ import sys
 input = sys.stdin.readline
 
 r = input().strip()
+n = len(r)
 
 x, y = 0, 0
 dir_num = 3
 
 time = 0
 answer = -1
-for i in r:
+for i in range(n):
 
     dxs, dys = [1, 0, -1, 0], [0, -1, 0, 1]
     time += 1
-
-    if i == 'L':
+    
+    if r[i] == 'L':
         dir_num = (dir_num - 1 + 4) % 4
-    if i == 'R':
+    if r[i] == 'R':
         dir_num = (dir_num + 1) % 4
-    if i == 'F':
+    if r[i] == 'F':
         x, y = x + dxs[dir_num], y + dys[dir_num]
 
     if x == 0 and y == 0:
         answer = time
+        break
 print(answer)
