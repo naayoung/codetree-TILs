1a4630876928af2b8358cd4d2ced81cb291f48f4
nayoung <ask5046a@gmail.com>
2024-07-04 18:04:25 +0900

    Title: 행복한 수열의 개수, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240704/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240704/행복한 수열의 개수/number-of-happy-sequence.py
@@ -6,16 +6,19 @@ graph = []
 for _ in range(n):
     graph.append(list(map(int, input().split())))
 
-count = 0
-graph2 = []
-for i in range(n):
-    row = []
-    for j in range(n):
-        row.append(graph[j][i])
-    graph2.append(row)
-    temp = set(graph[i])
-    temp2 = set(graph2[i])
+if m == 1:
+    print(n*(n-1))
+else:
+    count = 0
+    graph2 = []
+    for i in range(n):
+        row = []
+        for j in range(n):
+            row.append(graph[j][i])
+        graph2.append(row)
+        temp = set(graph[i])
+        temp2 = set(graph2[i])
 
-    if len(temp) < m+1 or len(temp2) < m+1:
-        count += 1
-print(count)
+        if len(temp) < m+1 or len(temp2) < m+1:
+            count += 1
+    print(count)
