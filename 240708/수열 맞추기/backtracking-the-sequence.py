9ffe99d7fa377e7f3f9edb7349f1da8a7bf7dea4
nayoung <ask5046a@gmail.com>
2024-07-08 20:02:33 +0900

    Title: 수열 맞추기, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240708/수열 맞추기/backtracking-the-sequence.py
+++ 240708/수열 맞추기/backtracking-the-sequence.py
@@ -4,15 +4,22 @@ input = sys.stdin.readline
 n = int(input().strip())
 a = list(map(int, input().split()))
 
-answer = []
+answer = [0] * n
 count = 0
 
-for i in reversed(a):
+for i in a:
     count += 1
     if i == 1:
-        answer.insert(0, count)
+        for j in range(n):
+            if answer[j] == 0:
+                answer[j] = count
+                break
     elif i == 2:
-        answer.insert(1, count)
+        for j in range(1, n):
+            if answer[j] == 0:
+                answer[j] = count
+                break
     else:
         answer.append(count)
+answer.reverse()
 print(*answer)
