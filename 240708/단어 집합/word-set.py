d065560382bad99eab21494bf9a5a4522cb1c8ea
nayoung <ask5046a@gmail.com>
2024-07-08 19:10:26 +0900

    Title: 단어 집합, Time: 118ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240708/단어 집합/word-set.py
+++ 240708/단어 집합/word-set.py
@@ -3,10 +3,15 @@ input = sys.stdin.readline
 
 while True:
     dic = input().strip()
+    answer = []
     if dic == "end":
         break
     else:
         dic_list = list(dic.split())
-        answer = list(set(dic_list))
+        for i in dic_list:
+            if i in answer:
+                continue
+            else:
+                answer.append(i)
         answer.sort()
         print(*answer)
