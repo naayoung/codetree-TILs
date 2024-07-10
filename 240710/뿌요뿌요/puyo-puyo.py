f16cfc762689f3d7947429fe65b5cb176602f9fc
nayoung <ask5046a@gmail.com>
2024-07-10 16:53:30 +0900

    Title: 뿌요뿌요, Time: 99ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240710/뿌요뿌요/puyo-puyo.py
@@ -0,0 +1,57 @@
+import sys
+sys.setrecursionlimit(10000)
+input = sys.stdin.readline
+
+n = int(input().strip())
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, input().split())))
+
+visited = [[0] * n for _ in range(n)]
+count = 0
+
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
+
+def can_go(x, y, k):
+    if not in_range(x, y):
+        return False
+    if visited[x][y] == 1 or graph[x][y] != k:
+        return False
+    return True
+
+def dfs(x, y, k):
+    global count
+    dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+
+        if can_go(nx, ny, k):
+            visited[nx][ny] = 1
+
+            dfs(nx, ny, k)
+
+def get_block(k):
+    global count, visited
+    count = 1
+    visited = [[0] * n for _ in range(n)]
+
+    for i in range(n):
+        for j in range(n):
+            if can_go(i, j, k):
+                visited[i][j] = 1
+                count += 1
+
+                dfs(i, j, k)
+
+answer = []
+block = 0
+for k in range(1, 101):
+    get_block(k)
+
+    if count >= 4:
+        block += 1
+    answer.append(count)
+
+print(block, max(answer))
