008f32b25092d8dfd9d0112c397a8bc2d253d235
nayoung <ask5046a@gmail.com>
2024-07-15 19:28:48 +0900

    Title: 갈 수 있는 곳들, Time: 240ms, Memory: 29MB, Status: Passed - CodeTree

--- 240715/갈 수 있는 곳들/places-can-go.py
+++ 240715/갈 수 있는 곳들/places-can-go.py
@@ -8,7 +8,7 @@ for _ in range(n):
     graph.append(list(map(int, input().split())))
 visited = [[0] * n for _ in range(n)]
 q = deque()
-count = 1
+count = 0
 
 def in_range(x, y):
     return 0 <= x and x < n and 0 <= y and y < n
@@ -21,7 +21,6 @@ def can_go(x, y):
     return True
 
 def bfs():
-    global count
     while q:
         x, y = q.popleft()
         dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
@@ -30,12 +29,17 @@ def bfs():
             nx, ny = x + dx, y + dy
             if can_go(nx, ny):
                 q.append((nx, ny))
-                visited[nx][ny] = 1
-                count += 1      
+                visited[nx][ny] = 1    
 
 for _ in range(k):
     r, c = map(int, input().split())
     q.append((r-1, c-1))
     visited[r-1][c-1] = 1
-    bfs()
+
+bfs()
+
+for i in range(n):
+    for j in range(n):
+        if visited[i][j]:
+            count += 1
 print(count)
