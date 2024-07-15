12e65c44025077c885378ad635b16c5b5555780e
nayoung <ask5046a@gmail.com>
2024-07-15 17:44:08 +0900

    Title: 연결된 그래프 2, Time: 1755ms, Memory: 103MB, Status: Passed - CodeTree

--- 240715/연결된 그래프 2/connected-graph-2.py
+++ 240715/연결된 그래프 2/connected-graph-2.py
@@ -6,15 +6,32 @@ n, m = map(int, input().split())
 graph = [[] for _ in range(n+1)]
 for _ in range(m):
     a, b = map(int, input().split())
-    graph[b].append(a)
+    graph[a].append(b)
+visited = [0] * (n+1)
+count = 0
+answer = []
 
-max_length = 0
-for i in graph:
-    if len(i) > max_length:
-        max_length = len(i)
+def dfs(v):
+    global count
+    visited[v] = 1
+    count += 1
 
-answer = 0
-for i in range(len(graph)):
-    if max_length == len(graph[i]):
-        answer = i
-print(*graph[answer])
+    for nv in graph[v]:
+        if not visited[nv]:
+            visited[nv] = 1
+            dfs(nv)
+
+for i in range(1, n+1):
+    count = 0
+    visited = [0] * (n+1)
+
+    dfs(i)
+    answer.append((i, count))
+
+# 갈 수 있는 최대 노드 수를 구합니다.
+maxNode = max(answer, key=lambda x: x[1])[1]
+
+# 갈 수 있는 최대 노드 수에 해당하는 노드 번호를 출력합니다.
+for node, count in answer:
+    if count == maxNode:
+        print(node, end=" ")
