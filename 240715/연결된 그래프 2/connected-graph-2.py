6a91dec88437219133091898cbb9f72ba3ee7ac0
nayoung <ask5046a@gmail.com>
2024-07-15 16:33:50 +0900

    Title: 연결된 그래프 2, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240715/연결된 그래프 2/connected-graph-2.py
@@ -0,0 +1,20 @@
+import sys
+sys.setrecursionlimit(10000)
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+
+max_length = 0
+for i in graph:
+    if len(i) > max_length:
+        max_length = len(i)
+
+answer = []
+for i in range(len(graph)):
+    if max_length == len(graph[i]):
+        answer.append(i)
+print(*answer)
