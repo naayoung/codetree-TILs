b383f0003f32e3732f01069fde374ccb710709a9
nayoung <ask5046a@gmail.com>
2024-07-18 18:06:32 +0900

    Title: 최소 경로로 탈출 하기, Time: 138ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240718/최소 경로로 탈출 하기/escape-with-min-distance.py
@@ -0,0 +1,41 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+graph = []
+for _ in range(n+1):
+    graph.append(list(map(int, input().split())))
+visited = [[0] * m for _ in range(n)]
+answer = [[0] * m for _ in range(n)]
+q = deque()
+
+def in_range(x, y):
+    return 0 <= x and x < m and 0 <= y and y < n
+
+def can_go(x, y):
+    if not in_range(x, y):
+        return False
+    if visited[x][y] == 1 or graph[x][y] == 0:
+        return False
+    return True
+
+def bfs():
+    global count
+
+    while q:
+        x, y = q.popleft()
+
+        dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x + dx, y + dy
+            if can_go(nx, ny):
+                visited[nx][ny] = 1
+                answer[nx][ny] = answer[x][y] + 1
+                q.append((nx, ny))
+
+q.append((0, 0))
+visited[0][0] = 1
+
+bfs()
+print(answer[n-1][m-1])
