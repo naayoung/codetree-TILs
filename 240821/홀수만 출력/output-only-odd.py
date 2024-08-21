3451d86c7badba96f937a9f54889b09e96d19244
nayoung <ask5046a@gmail.com>
2024-08-21 19:05:04 +0900

    Title: 홀수만 출력, Time: 230ms, Memory: 24MB, Status: Passed - CodeTree

--- 240821/홀수만 출력/output-only-odd.py
+++ 240821/홀수만 출력/output-only-odd.py
@@ -1,8 +1,7 @@
 a, b = map(int, input().split())
 num = []
 
-for i in range(a, b+1):
-    if i%2 != 0:
-        num.append(i)
+for i in range(a, b+1, 2):
+    num.append(i)
 num.sort()
 print(*num)
