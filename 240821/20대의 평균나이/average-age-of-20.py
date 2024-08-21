32ac8f7864e5fe3ab21ca1cb5314d781da177abf
nayoung <ask5046a@gmail.com>
2024-08-21 19:51:03 +0900

    Title: 20대의 평균나이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240821/20대의 평균나이/average-age-of-20.py
@@ -0,0 +1,10 @@
+num = []
+
+while True:
+    age = int(input())
+    if age < 30:
+        num.append(age)
+    else:
+        answer = sum(num)/len(num)
+        print(f"{answer:.2f}")
+        break
