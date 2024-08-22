56e2e55dcbad96b19c69b8f4565e1351a92819a7
nayoung <ask5046a@gmail.com>
2024-08-23 01:12:16 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 149ms, Memory: 25MB, Status: Passed - CodeTree

--- 240823/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
+++ 240823/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -2,14 +2,13 @@ n = int(input())
 num = list(map(int, input().split()))
 
 answer = []
-max_num = max(num)
-max_num_index = num.index(max(num)) + 1
-if max_num_index == 1:
-    answer.append(max_num_index)
-while max_num_index > 1:
+while True:
     max_num = max(num)
     max_num_index = num.index(max(num)) + 1
     answer.append(max_num_index)
 
     num = num[0:max_num_index-1]
+
+    if max_num_index == 1:
+        break
 print(*answer)
