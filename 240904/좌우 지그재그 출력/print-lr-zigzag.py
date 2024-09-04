eed94f92436199d5e49fa36fd655ca080be6db1c
nayoung <ask5046a@gmail.com>
2024-09-04 20:52:15 +0900

    Title: 좌우 지그재그 출력, Time: 129ms, Memory: 26MB, Status: Passed - CodeTree

--- 240904/좌우 지그재그 출력/print-lr-zigzag.py
+++ 240904/좌우 지그재그 출력/print-lr-zigzag.py
@@ -6,13 +6,13 @@ for i in range(1, n+1):
         #홀수
         if i%2 == 1:
             if i > 1 and j == 0:
-                cnt = cnt*2 - 1
+                cnt = cnt+n
             else:
                 cnt += 1
         #짝수
         else:
             if j == 0:
-                cnt = cnt*2
+                cnt = cnt+n
             else:
                 cnt -= 1
         print(cnt, end=" ")
