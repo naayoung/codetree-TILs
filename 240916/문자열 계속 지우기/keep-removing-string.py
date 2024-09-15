6c2bb257ea1ce337470ece60b590d26d83288cd9
naayoung <ask5046a@gmail.com>
2024-09-16 01:56:58 +0900

    Title: 문자열 계속 지우기, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

--- 240916/문자열 계속 지우기/keep-removing-string.py
+++ 240916/문자열 계속 지우기/keep-removing-string.py
@@ -1,10 +1,10 @@
 a = input()
 b = input()
 
-while True:
-    if b in a:
-        answer = a.replace(b, "")
-        a = answer
-    else:
-        break
-print(answer)
+while b in a:
+    # 가장 앞에 등장하는 B의 시작 위치를 찾기
+    index = a.find(b)
+    # 찾은 부분을 삭제
+    a = a[:index] + a[index+len(b):]
+
+print(a)
