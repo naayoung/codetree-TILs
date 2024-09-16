b3ba92fb3f050eb9e7609a4116a8f2464d158b28
naayoung <ask5046a@gmail.com>
2024-09-16 22:17:51 +0900

    Title: 함수를 이용한 소수 판별, Time: 121ms, Memory: 25MB, Status: Passed - CodeTree

--- 240916/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 240916/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -3,9 +3,12 @@ a, b = map(int, input().split())
 temp = [i for i in range(a, b+1)]
 for i in range(a, b+1):
     cnt = 0
-    for j in range(2, 10):
+    for j in range(1, i):
         if i % j == 0:
             cnt += 1
     if cnt > 1:
         temp.remove(i)
-print(sum(temp))
+if a == 1:
+    print(0)
+else:
+    print(sum(temp))
