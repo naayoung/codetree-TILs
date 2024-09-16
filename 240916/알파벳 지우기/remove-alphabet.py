0de7948dd9bb8e03c25057870f62219376fc327b
naayoung <ask5046a@gmail.com>
2024-09-16 18:47:08 +0900

    Title: 알파벳 지우기, Time: 163ms, Memory: 24MB, Status: Passed - CodeTree

--- 240916/알파벳 지우기/remove-alphabet.py
+++ 240916/알파벳 지우기/remove-alphabet.py
@@ -1,15 +1,15 @@
-w1 = input().upper()
-w2 = input().upper()
+w1 = input()
+w2 = input()
 
 answer1 = []
 answer2 = []
 for i in w1:
-    if i >= '0' and i <= '8':
+    if i >= '0' and i <= '9':
         answer1.append(i)
 answer1 = int(''.join(answer1))
 
 for i in w2:
-    if i >= '0' and i <= '8':
+    if i >= '0' and i <= '9':
         answer2.append(i)
 answer2 = int(''.join(answer2))
 
