3e2eee7c96c0082185e8e258b269cd3059b5f4e9
naayoung <ask5046a@gmail.com>
2024-09-21 01:58:01 +0900

    Title: 2021년 날짜의 유무, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 240921/2021년 날짜의 유무/with-or-without-2021.py
+++ 240921/2021년 날짜의 유무/with-or-without-2021.py
@@ -5,6 +5,8 @@ def cal(M, D):
         return False
     elif M == 2 and D >= 28:
         return False
+    elif M == 8 and D == 31:
+        return True
     elif M%2 == 0 and D == 31:
         return False
     else:
