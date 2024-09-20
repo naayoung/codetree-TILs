358961e03b8690dbc904fa8af7012692dcd46034
naayoung <ask5046a@gmail.com>
2024-09-21 01:56:14 +0900

    Title: 2021년 날짜의 유무, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240921/2021년 날짜의 유무/with-or-without-2021.py
+++ 240921/2021년 날짜의 유무/with-or-without-2021.py
@@ -1,7 +1,9 @@
 M, D = map(int, input().split())
 
 def cal(M, D):
-    if M == 2 and D >= 28:
+    if M > 12:
+        return False
+    elif M == 2 and D >= 28:
         return False
     elif M%2 == 0 and D == 31:
         return False
