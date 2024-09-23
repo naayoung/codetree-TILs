a894c83f438aba00cf384c8acd0278974d2e8f3e
naayoung <ask5046a@gmail.com>
2024-09-24 02:27:53 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240924/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
+++ 240924/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -12,12 +12,6 @@ def gcd(a, b):
 def lcm(a, b):
     return a * b // gcd(a, b)
 
-# 리스트에서 첫 번째부터 끝까지의 최대공약수를 재귀적으로 구하는 함수
-def find_gcd_of_list(nums, idx):
-    if idx == 0:
-        return nums[0]  # 재귀의 기본 경우
-    return gcd(nums[idx], find_gcd_of_list(nums, idx - 1))
-
 # 리스트에서 첫 번째부터 끝까지의 최소공배수를 재귀적으로 구하는 함수
 def find_lcm_of_list(nums, idx):
     if idx == 0:
