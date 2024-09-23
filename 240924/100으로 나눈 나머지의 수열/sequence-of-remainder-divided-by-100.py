77ca58f767943714bdb9e0cfde284b83818a59fa
naayoung <ask5046a@gmail.com>
2024-09-24 01:52:13 +0900

    Title: 100으로 나눈 나머지의 수열, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

--- 240924/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.py
+++ 240924/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.py
@@ -3,6 +3,10 @@ n = int(input())
 def sol(n, a, b, i):
     if n == i:
         return (a*b)%100
+    if n == 1:
+        return 2
+    if n == 2:
+        return 4
     return sol(n, b, (a*b)%100, i+1)
 
 print(sol(n, 2, 4, 3))
