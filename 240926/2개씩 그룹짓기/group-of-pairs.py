2fd7494f109812934cd80de6df337261e413f916
naayoung <ask5046a@gmail.com>
2024-09-26 01:02:07 +0900

    Title: 2개씩 그룹짓기, Time: 111ms, Memory: 25MB, Status: Passed - CodeTree

--- 240926/2개씩 그룹짓기/group-of-pairs.py
+++ 240926/2개씩 그룹짓기/group-of-pairs.py
@@ -13,3 +13,15 @@ while num:
     if num_sum > max_sum:
         max_sum = num_sum
 print(max_sum)
+
+'''
+group_max = 0
+for i in range(n):
+    # i번째와 2n - 1 - i번째 원소를 매칭합니다.
+    group_sum = nums[i] + nums[2*n - 1 - i]
+    if group_sum > group_max:
+        # 최댓값을 갱신합니다.
+        group_max = group_sum
+
+print(group_max)
+'''
