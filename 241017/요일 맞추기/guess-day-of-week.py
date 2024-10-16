d7de85581d04304343b2157b96ca0f03888d87d2
naayoung <ask5046a@gmail.com>
2024-10-17 02:25:53 +0900

    Title: 요일 맞추기, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241017/요일 맞추기/guess-day-of-week.py
+++ 241017/요일 맞추기/guess-day-of-week.py
@@ -3,15 +3,22 @@ num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 day_of_week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
 if m1 == m2:
-    if d2-d1 >= 0:
+    if d2-d1 >= 0 and d2-d1 < 7:
         answer = day_of_week[d2-d1]
+    elif d2-d1 >= 7:
+        answer = day_of_week[(d2-d1)%7]
     else:
         answer = day_of_week[7+d2-d1]
 else:
-    temp = num_of_days[m2]-num_of_days[m1]
-    tt = (d2-d1)-(m2-m1)+temp
-    if tt >= 0:
-        answer = day_of_week[tt]
+    temp = (d2-d1)-(m2-m1)+abs(m2-m1)
+    
+    if temp >= 0 and temp < 7:
+        answer = day_of_week[temp]
+    elif temp >= 7:
+        if m2 == 2:
+            answer = day_of_week[temp%7-2]
+        else:
+            answer = day_of_week[temp%7]
     else:
-        answer = day_of_week[7+tt]
+        answer = day_of_week[7+temp]
 print(answer)
