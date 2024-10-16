752751f14dc1100fbdb52e614489c8e64e08f9d1
naayoung <ask5046a@gmail.com>
2024-10-17 03:21:09 +0900

    Title: 그 요일은, Time: 203ms, Memory: 24MB, Status: Passed - CodeTree

--- 241017/그 요일은/the-day-of-the-day.py
+++ 241017/그 요일은/the-day-of-the-day.py
@@ -4,19 +4,15 @@ A = input()
 
 
 def num_of_days(m, d):
-    # 계산 편의를 위해 월마다 몇 일이 있는지를 적어줍니다. 
     days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     total_days = 0
     
-    # 1월부터 (m - 1)월 까지는 전부 꽉 채워져 있습니다.
     for i in range(1, m):
         total_days += days[i]
     
-    # m월의 경우에는 정확이 d일만 있습니다.
     total_days += d
     
     return total_days
-
 
 def num_of_day(s):
     # 간단한 비교를 위해 요일을 숫자로 나타내줍니다.
