172a7cfb2101d748627c986da846004a86d5e963
naayoung <ask5046a@gmail.com>
2024-10-17 03:19:48 +0900

    Title: 그 요일은, Time: 154ms, Memory: 24MB, Status: Passed - CodeTree

--- 241017/그 요일은/the-day-of-the-day.py
+++ 241017/그 요일은/the-day-of-the-day.py
@@ -1,23 +1,51 @@
-m1, d1, m2, d2 = map(int, input().split())
-weekDay = input()
-lst = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-days = 0
+# 변수 선언 및 입력
+m1, d1, m2, d2 = tuple(map(int, input().split()))
+A = input()
 
 
-def countDays(month):
-    result = 0
-    if month in [1, 3, 5, 7, 8, 10, 12]:
-        result += 31
-    elif month == 2:
-        result += 29
-    else:
-        result += 30
-    return result
+def num_of_days(m, d):
+    # 계산 편의를 위해 월마다 몇 일이 있는지를 적어줍니다. 
+    days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    total_days = 0
+    
+    # 1월부터 (m - 1)월 까지는 전부 꽉 채워져 있습니다.
+    for i in range(1, m):
+        total_days += days[i]
+    
+    # m월의 경우에는 정확이 d일만 있습니다.
+    total_days += d
+    
+    return total_days
 
 
-for i in range(m1, m2):
-    days += countDays(i)
+def num_of_day(s):
+    # 간단한 비교를 위해 요일을 숫자로 나타내줍니다.
+    if s == "Mon": 
+        return 0
+    elif s == "Tue": 
+        return 1
+    elif s == "Wed": 
+        return 2
+    elif s == "Thu": 
+        return 3
+    elif s == "Fri": 
+        return 4
+    elif s == "Sat": 
+        return 5
+    return 6
 
-days = days - (d1 + lst.index(weekDay)) + d2
 
-print((days // 7)+1)
+ans = 0   
+start_date = num_of_days(m1, d1)
+end_date = num_of_days(m2, d2)
+cur_day = num_of_day("Mon")
+
+for date in range(start_date, end_date + 1):
+    # 오늘의 요일이 A요일과 같다면 정답에 추가합니다.
+    if cur_day == num_of_day(A): 
+        ans += 1
+
+    cur_day = (cur_day + 1) % 7
+
+# 출력
+print(ans)
