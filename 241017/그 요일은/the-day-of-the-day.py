4421aa9755e5505a73472e81463acb078e0ef660
naayoung <ask5046a@gmail.com>
2024-10-17 03:19:28 +0900

    Title: 그 요일은, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

--- 241017/그 요일은/the-day-of-the-day.py
+++ 241017/그 요일은/the-day-of-the-day.py
@@ -1,39 +1,23 @@
 m1, d1, m2, d2 = map(int, input().split())
-A = input()
+weekDay = input()
+lst = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+days = 0
 
-# 요일 배열 (0: Mon, 1: Tue, 2: Wed, 3: Thu, 4: Fri, 5: Sat, 6: Sun)
-day_of_week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-days_in_month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-# 날짜의 총 일수를 계산하는 함수
-def num_of_days(m, d):
-    total_days = 0
-    for i in range(1, m):
-        total_days += days_in_month[i]
-    total_days += d
-    return total_days
+def countDays(month):
+    result = 0
+    if month in [1, 3, 5, 7, 8, 10, 12]:
+        result += 31
+    elif month == 2:
+        result += 29
+    else:
+        result += 30
+    return result
 
-# 시작일까지의 총 일수
-start_days = num_of_days(m1, d1)
-# 2024년 1월 1일은 월요일이므로 요일 인덱스는 0
-current_day_index = start_days % 7
 
-# A 요일의 등장 횟수 카운트
-count_A = 0
+for i in range(m1, m2):
+    days += countDays(i)
 
-# 날짜 범위 내에서 A 요일의 등장 횟수 계산
-for m in range(m1, m2 + 1):
-    for d in range(1, days_in_month[m] + 1):
-        if m == m1 and d < d1:
-            continue  # 시작 날짜 이전의 날짜는 건너뜀
-        if m == m2 and d > d2:
-            break  # 끝 날짜 이후의 날짜는 건너뜀
+days = days - (d1 + lst.index(weekDay)) + d2
 
-        # 현재 요일이 A와 같으면 카운트 증가
-        if day_of_week[current_day_index] == A:
-            count_A += 1
-        
-        # 요일 인덱스 업데이트
-        current_day_index = (current_day_index + 1) % 7
-
-print(count_A)
+print((days // 7)+1)
