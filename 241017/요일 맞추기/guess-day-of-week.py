39b405f9ba72ac02f5efd4634e18488cfca7f03e
naayoung <ask5046a@gmail.com>
2024-10-17 02:43:09 +0900

    Title: 요일 맞추기, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 241017/요일 맞추기/guess-day-of-week.py
+++ 241017/요일 맞추기/guess-day-of-week.py
@@ -1,24 +1,21 @@
 m1, d1, m2, d2 = map(int, input().split())
-num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-day_of_week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+day_of_week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 
-if m1 == m2:
-    if d2-d1 >= 0 and d2-d1 < 7:
-        answer = day_of_week[d2-d1]
-    elif d2-d1 >= 7:
-        answer = day_of_week[(d2-d1)%7]
-    else:
-        answer = day_of_week[7+d2-d1]
-else:
-    temp = (num_of_days[m1]-d1+1) + d2 + sum(num_of_days[m1+1:m2])
-    #print(temp)
-    if temp >= 0 and temp < 7:
-        answer = day_of_week[temp]
-    elif temp >= 7:
-        if m2 > m1:
-            answer = day_of_week[temp%7-1]
-        else:
-            answer = day_of_week[temp%7+1]
-    else:
-        answer = day_of_week[7+temp]
-print(answer)
+def num_of_days(m, d):
+    # 계산 편의를 위해 월마다 몇 일이 있는지를 적어줍니다. 
+    days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    total_days = 0
+    
+    # 1월부터 (m - 1)월 까지는 전부 꽉 채워져 있습니다.
+    for i in range(1, m):
+        total_days += days[i]
+    
+    # m월의 경우에는 정확이 d일만 있습니다.
+    total_days += d
+    
+    return total_days    
+
+
+# 출력
+total_days = num_of_days(m2, d2) - num_of_days(m1, d1) + 1
+print(day_of_week[total_days%7])
