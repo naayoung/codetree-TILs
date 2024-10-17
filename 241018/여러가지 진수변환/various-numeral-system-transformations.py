dd192fdb9fb92f1d477211a35237c99e0bf5156a
naayoung <ask5046a@gmail.com>
2024-10-18 01:23:48 +0900

    Title: 여러가지 진수변환, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241018/여러가지 진수변환/various-numeral-system-transformations.py
@@ -0,0 +1,15 @@
+n, b = map(int, input().split())
+answer = []
+
+#b진수로 변경
+while True:
+    if n < b:
+        answer.append(str(n))
+        break
+    answer.append(str(n%b))
+    n = n//b
+
+#출력
+answer.sort(reverse=False)
+answer = ''.join(answer)
+print(answer)
