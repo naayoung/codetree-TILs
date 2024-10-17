35df8d8321d85cc4af0ace0046523e09e9951015
naayoung <ask5046a@gmail.com>
2024-10-18 01:39:36 +0900

    Title: 진수 to 진수, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241018/진수 to 진수/transformation-of-number-system.py
@@ -0,0 +1,20 @@
+a, b = map(int, input().split())
+n = list(map(int, input()))
+nn = 0
+answer = []
+
+#a진수에서 10진수로 변경
+for i in range(len(n)):
+    nn += n[i]*(a**i)
+
+#10진수에서 b진수로 변경
+while True:
+    if nn < b:
+        answer.append(nn)
+        break
+    answer.append(nn%b)
+    nn = nn//b
+
+#출력
+for i in answer[::-1]:
+    print(i, end="")
