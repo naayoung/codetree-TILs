4742dd5bbd054b133179862184b1c74425383b54
naayoung <ask5046a@gmail.com>
2024-10-23 00:53:45 +0900

    Title: 흰검 칠하기, Time: 232ms, Memory: 30MB, Status: Passed - CodeTree

--- 241023/흰검 칠하기/painting-white-black.py
+++ 241023/흰검 칠하기/painting-white-black.py
@@ -10,19 +10,19 @@ for _ in range(n):
 
     if b == 'L':
         for i in range(m-a+1, m+1):
+            temp1[i] += 1
             if temp1[i] >= 2 and temp2[i] >= 2:
                 color[i] = 'G'
             if color[i] != 'G':
                 color[i] = 'W'
-            temp1[i] += 1
         m = m-a+1
     else:
         for i in range(m, m+a):
+            temp2[i] += 1
             if temp1[i] >= 2 and temp2[i] >= 2:
                 color[i] = 'G'
             if color[i] != 'G':
                 color[i] = 'B'
-            temp2[i] += 1
         m = m+a-1
 
 c1, c2, c3 = 0, 0, 0
