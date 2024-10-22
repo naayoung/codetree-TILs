df5e395d725c095a629a531d1489f13d4263f68f
naayoung <ask5046a@gmail.com>
2024-10-23 00:41:08 +0900

    Title: 흰검 칠하기, Time: 138ms, Memory: 29MB, Status: Wrong Answer - CodeTree

---
+++ 241023/흰검 칠하기/painting-white-black.py
@@ -0,0 +1,36 @@
+n = int(input())
+
+temp1 = [0]*200001
+temp2 = [0]*200001
+color = ['']*200001
+m = 100000
+for _ in range(n):
+    a, b = input().split()
+    a = int(a)
+
+    if b == 'L':
+        for i in range(m-a+1, m+1):
+            if temp1[i] >= 2 or temp2[i] >= 2:
+                color[i] = 'G'
+            else:
+                color[i] = 'W'
+            temp1[i] += 1
+        m = m-a+1
+    else:
+        for i in range(m, m+a):
+            if temp1[i] >= 2 or temp2[i] >= 2:
+                color[i] = 'G'
+            else:
+                color[i] = 'B'
+            temp2[i] += 1
+        m = m+a-1
+
+c1, c2, c3 = 0, 0, 0
+for i in color:
+    if i == 'W':
+        c1 += 1
+    if i == 'B':
+        c2 += 1
+    if i == 'G':
+        c3 += 1
+print(c1, c2, c3)
