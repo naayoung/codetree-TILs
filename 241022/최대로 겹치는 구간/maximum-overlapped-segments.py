22d8651fdd72450a17fe6e26b0c4f1c26c87815a
naayoung <ask5046a@gmail.com>
2024-10-22 23:01:37 +0900

    Title: 최대로 겹치는 구간, Time: 107ms, Memory: 25MB, Status: Passed - CodeTree

--- 241022/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 241022/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -3,6 +3,7 @@ n = int(input())
 answer = [0] * 201
 for _ in range(n):
     x1, x2 = map(int, input().split())
+    x1, x2 = x1+100, x2+100
     for i in range(x1, x2):
         answer[i] += 1
 print(max(answer))
