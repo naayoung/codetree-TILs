a58f0bb5d667f5c76ad1df27b98b063994d10be2
naayoung <ask5046a@gmail.com>
2024-10-23 00:12:38 +0900

    Title: 신기한 타일 뒤집기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241023/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -0,0 +1,23 @@
+n = int(input())
+
+color = ['']*2001
+m = 1000
+for _ in range(n):
+    a, b = input().split()
+    a = int(a)
+
+    if b == 'L':
+        for i in range(m-a, m):
+            color[i] = 'W'
+        m = m-a
+    else:
+        for i in range(m, m+a):
+            color[i] = 'B'
+        m = m+a
+ans = [0, 0]
+for i in color:
+    if i == 'W':
+        ans[0] += 1
+    if i == 'B':
+        ans[1] += 1
+print(*ans)
