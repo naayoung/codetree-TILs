1eea0ffaad0fc5bfac474807f35b8d51644c5738
naayoung <ask5046a@gmail.com>
2024-10-29 02:31:10 +0900

    Title: 연속되는 수 3, Time: 130ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 241029/연속되는 수 3/continuous-number3.py
@@ -0,0 +1,10 @@
+n = int(input())
+temp = [int(input()) for _ in range(n)]
+answer, cnt = 0, 0
+for i in range(len(temp)):
+    if (temp[i] > 0 and temp[i-1] > 0) or (temp[i] < 0 and temp[i-1] < 0):
+        cnt += 1
+    else:
+        cnt = 1
+    answer = max(cnt, answer)
+print(answer)
