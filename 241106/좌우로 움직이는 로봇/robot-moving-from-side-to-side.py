ad0f19f9b82e9a41810c5c62be4bdaac8e157509
naayoung <ask5046a@gmail.com>
2024-11-06 05:04:12 +0900

    Title: 좌우로 움직이는 로봇, Time: 229ms, Memory: 53MB, Status: Passed - CodeTree

--- 241106/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 241106/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -1,42 +1,38 @@
 n, m = map(int, input().split())
 
-a, b = {}, {}
-time_a, time_b = 0, 0
+a, b = [], []
 distance_a, distance_b = 0, 0
 #a가 움직인 횟수
 for _ in range(n):
     t, d = input().split()
     t = int(t)
     for i in range(t):
-        time_a += 1
         if d == 'L':
             distance_a -= 1
         if d == 'R':
             distance_a += 1
-        a[time_a] = distance_a
+        a.append(distance_a)
 #b가 움직인 횟수
 for _ in range(m):
     t, d = input().split()
     t = int(t)
     for i in range(t):
-        time_b += 1
         if d == 'L':
             distance_b -= 1
         if d == 'R':
             distance_b += 1
-        b[time_b] = distance_b
+        b.append(distance_b)
 
-max_time = max(time_a, time_b)
+# 두 리스트 중 짧은 것을 기준으로 자르고 나머지는 마지막 위치로 고정
+max_time = max(len(a), len(b))
+while len(a) < max_time:
+    a.append(a[-1])
+while len(b) < max_time:
+    b.append(b[-1])
+
+# 로봇들이 같은 위치에 오게 되는 순간을 찾기 위한 로직
 answer = 0
-for i in range(1, max_time+1):
-    if i > time_a:
-        if a[time_a] == b[i] and a[time_a] != b[i-1]:
-            answer += 1
-    elif i > time_b:
-        if a[i] == b[time_b] and a[i-1] != b[time_b]:
-            answer += 1
-    else:
-        if a[i] == b[i] and a[i-1] != b[i-1]:
-            answer += 1
-
+for i in range(1, max_time):
+    if a[i] == b[i] and a[i - 1] != b[i - 1]:
+        answer += 1
 print(answer)
