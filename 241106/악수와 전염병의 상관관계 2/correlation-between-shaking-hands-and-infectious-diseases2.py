03c8efd757a08f8096d2e8e1b6405591ae2d875b
naayoung <ask5046a@gmail.com>
2024-11-06 04:03:52 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 49ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241106/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 241106/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -8,8 +8,9 @@ temp.sort(key = lambda x:x[0])
 
 cnt = 0
 people = [0]*N
+people[P-1] = 1
 for i in temp:
-    if i[1] == P or i[2] == P:
+    if people[i[1]-1] == 1 or people[i[2]-1] == 1:
         cnt += 1
         if cnt == T:
             break
