c0fb763f202db1663f2ec9a7f21afa081fe23c90
naayoung <ask5046a@gmail.com>
2024-11-05 20:27:47 +0900

    Title: 계속 중첩되는 사각형, Time: 59ms, Memory: 17MB, Status: Passed - CodeTree

--- 241105/계속 중첩되는 사각형/continuously-overlapping-squares.py
+++ 241105/계속 중첩되는 사각형/continuously-overlapping-squares.py
@@ -1,10 +1,23 @@
 n = int(input())
-answer = 0
 
 offset = 100
-for _ in range(n):
+max_r = 200
+box = [[0 for col in range(max_r+1)] for row in range(max_r+1)]
+
+for nn in range(n):
     x1, y1, x2, y2 = map(int,input().split())
     x1, y1, x2, y2 = x1+offset, y1+offset, x2+offset, y2+offset
 
-    answer = (x2-x1)*(y2-y1)
-print(answer)
+    for i in range(x1, x2):
+        for j in range(y1, y2):
+            if nn%2 == 0:
+                box[i][j] = 0
+            else:
+                box[i][j] = 1
+
+ans = 0
+for x in range(max_r+1):
+    for y in range(max_r+1):
+        if box[x][y]:
+            ans += 1
+print(ans)
