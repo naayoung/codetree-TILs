6b2d34c6ad0a958052415c3591631cd32e014667
naayoung <ask5046a@gmail.com>
2024-12-01 19:50:26 +0900

    Title: 되돌아오기, Time: 40ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241201/되돌아오기/come-back.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+dir = {
+    'W':2,
+    'S':1,
+    'N':3,
+    'E':0
+}
+
+n = int(input().strip())
+
+x, y = 0, 0
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+
+ans = -1
+time = 0
+for _ in range(n):
+    dr, dd = input().split()
+    dr, dd = dir[dr], int(dd)
+
+    for _ in range(dd):
+        nx, ny = x+dx[dr], y+dy[dr]
+        x, y = nx, ny
+        time += 1
+        if x == 0 and y == 0:
+            ans = time
+            break
+
+print(ans)
+
