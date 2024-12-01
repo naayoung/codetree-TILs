68aa710dd7a53d92a3d67d943f72326c5ac755d7
naayoung <ask5046a@gmail.com>
2024-12-01 19:57:27 +0900

    Title: 되돌아오기, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 241201/되돌아오기/come-back.py
+++ 241201/되돌아오기/come-back.py
@@ -15,17 +15,19 @@ dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
 
 ans = -1
 time = 0
+done = False
 for _ in range(n):
     dr, dd = input().split()
     dr, dd = dir[dr], int(dd)
 
     for _ in range(dd):
         x, y = x+dx[dr], y+dy[dr]
-
         time += 1
+
         if x == 0 and y == 0:
             ans = time
-            break
+            done = True
+    if done:
         break
 
 print(ans)
