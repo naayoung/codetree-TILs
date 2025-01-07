78a2799c87968f1e8e50a075af91666a9609a3a1
naayoung <ask5046a@gmail.com>
2025-01-07 15:11:53 +0900

    Title: 씨 오 더블유 2, Time: 52ms, Memory: 16MB, Status: Passed - CodeTree

--- 250107/씨 오 더블유 2/c-o-w-2.py
+++ 250107/씨 오 더블유 2/c-o-w-2.py
@@ -3,11 +3,19 @@ str = input()
 
 # Write your code here!
 c, o, w = [], [], []
-for s in range(len(str)):
+for s in range(n):
     if str[s] == 'C':
         c.append(s+1)
     if str[s] == 'O':
         o.append(s+1)
     if str[s] == 'W':
         w.append(s+1)
-print(len(c)*len(o)*len(w))
+
+answer = 0
+for cc in c:
+    for oo in o:
+        if oo > cc:
+            for ww in w:
+                if ww > oo:
+                    answer += 1
+print(answer)
