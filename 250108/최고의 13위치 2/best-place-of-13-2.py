5050755d4cc884ef051c6cd0ce933248d6ca54b8
naayoung <ask5046a@gmail.com>
2025-01-08 13:37:52 +0900

    Title: 최고의 13위치 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250108/최고의 13위치 2/best-place-of-13-2.py
+++ 250108/최고의 13위치 2/best-place-of-13-2.py
@@ -4,9 +4,11 @@ arr = [list(map(int, input().split())) for _ in range(n)]
 # Write your code here!
 ans = []
 for a in arr:
+    t = 0
     for i in range(n):
         temp = a[i:i+3]
-        if len(temp) == 3:
-            ans.append(sum(temp))
+        if len(temp) == 3 and sum(temp) >= 0:
+            t = sum(temp)
+    ans.append(t)
 ans.sort(reverse=True)
 print(ans[0]+ans[1])
