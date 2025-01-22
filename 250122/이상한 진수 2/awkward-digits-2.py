e94eb7a81800c4a1d0997e7b8cb0f67aabc558d3
naayoung <ask5046a@gmail.com>
2025-01-22 17:48:28 +0900

    Title: 이상한 진수 2, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250122/이상한 진수 2/awkward-digits-2.py
+++ 250122/이상한 진수 2/awkward-digits-2.py
@@ -7,13 +7,13 @@ ans = [0]
 temp = 0
 for i in range(aa):
     if A[i] == '1':
-        temp += 2**abs(i-3)
+        temp += 2**abs(i-(aa-1))
 
 for i in range(1, aa):
     tmp = temp
     if A[i] == '1':
-        tmp = temp - 2**abs(i-3)
+        tmp = temp - 2**abs(i-(aa-1))
     else:
-        tmp = temp + 2**abs(i-3)
+        tmp = temp + 2**abs(i-(aa-1))
     ans.append(tmp)
 print(max(ans))
