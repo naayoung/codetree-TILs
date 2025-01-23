bdefb4b0d5b2a2239e1179e018d8675a88f38154
naayoung <ask5046a@gmail.com>
2025-01-23 10:56:32 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 55ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 250123/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -0,0 +1,18 @@
+n = int(input())
+points = [tuple(map(int, input().split())) for _ in range(n)]
+x = [p[0] for p in points]
+y = [p[1] for p in points]
+
+# Write your code here!
+ans = []
+for i in range(1, n-1):
+    ax, ay = 0, 0
+    temp = 0
+    for j in range(1, n):
+        if i != j:
+            temp += abs(ax-x[j]) + abs(ay-y[j])
+            ax, ay = x[j], y[j]
+
+    ans.append(temp)
+
+print(min(ans))
