b3160ce0cd9c90a4635f6eb7e672664fcdc31955
naayoung <ask5046a@gmail.com>
2025-02-21 21:25:43 +0900

    Title: 바구니 안의 사탕 2, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250221/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -0,0 +1,15 @@
+N, K = map(int, input().split())
+candy = []
+pos = [0]*101
+
+for _ in range(N):
+    c, p = map(int, input().split())
+    pos[p] = c
+# Write your code here!
+ans = 0
+for c in range(101-K):
+    temp = pos[c-K:c+K+1]
+    if ans < sum(temp):
+        ans = sum(temp)
+print(ans)
+
