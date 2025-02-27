df86aab8b54019275091c1a75ffce722802daaa9
naayoung <ask5046a@gmail.com>
2025-02-27 22:59:11 +0900

    Title: 밭의 높이를 고르게하기, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250227/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
+++ 250227/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -1,3 +1,5 @@
+import sys
+
 N, H, T = map(int, input().split())
 num = list(map(int, input().split()))
 
@@ -5,7 +7,7 @@ num = list(map(int, input().split()))
 #1. T만큼 자르기
 #2. 자른 구간에 최소비용 계산하기
 
-ans = 100000
+ans = sys.maxsize
 for i in range(0, N-T+1):
     temp_ans = 0
     temp = num[i:i+T]
